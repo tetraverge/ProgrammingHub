2d73a9f5c0c90ac18c8c2e1615a3a4dea2d199b4
rabbii <rabbiulipur@gmail.com>
2021-03-19 15:50:03 +0600

    New Update

--- app/src/main/java/com/hub/programminghub/Quiz_Activity.java
+++ app/src/main/java/com/hub/programminghub/Quiz_Activity.java
@@ -99,6 +99,7 @@ public class Quiz_Activity extends AppCompatActivity implements View.OnClickList
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         startActivity(new Intent(getApplicationContext(), Quiz_Activity.class));
+                        finish();
                     }
                 })
                 .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
